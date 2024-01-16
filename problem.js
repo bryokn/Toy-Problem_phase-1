cbafc064f75e1563d255d86f14114d26e3d6847f
Brian Kipkirui <bryokn@gmail.com>
2024-01-16 13:16:19 +0300

    Update problem.js
    I added console.log statements to output the grade, and fixed the typo in the grade assigned for the 'C' range.
    
    I also correctted this range "else if(marks>=59 && marks<=49)" to else if(marks>=50 && marks<=59)

--- problem.js
+++ problem.js
@@ -1,26 +1,28 @@
 let marks;
 //declare variable
-//  A > 79, B - 60 to 79, C -  59 to 49,
-// D - 40 to 49, E - less 40.
+//  A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40.
+//User to input their marks
 marks=prompt(`input your marks`);
- if(marks>0 && marks<100){
+//Check if input is between 0-100 
+if(marks>0 && marks<100){
+ //Check grade based on ranges below
 if(marks>79){
-    `A`;
+    console.log(`A`);
  }
  else if(marks>=60 && marks<=79){
-    `B`
+    console.log(`B`);
  }
-else if(marks>=59 && marks<=49){
-    `c`
+else if(marks>=50 && marks<=59){
+    console.log(`C`);
 }
 else if(marks>=40 && marks<=49){
-    `D`
+    console.log(`D`);
 }
 else if(marks<40){
-    `E`
+    console.log(`E`);
 
     }
 }
 else {
-   alert(`your results are invalid input between 0 and 100`); 
+   alert(`Your results are invalid. Input between 0 and 100`); //Display alert if input is not within range
 }
